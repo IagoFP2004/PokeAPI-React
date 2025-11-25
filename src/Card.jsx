865fe2f7cb2250e728e847cce84ee5573f27dfcb
IagoFP2004 <iagofranciscoperez123@gmail.com>
2025-11-25 16:41:13 +0100

    Agregados estilos a la paginacion

--- src/Card.jsx
+++ src/Card.jsx
@@ -77,9 +77,13 @@ export function Card() {
         ))}
       </div>
       <div className="pagination">
-        <button onClick={prevPage} disabled={page === 0}>Anterior</button>
-        <span>Página {page + 1}</span>
-        <button onClick={nextPage}>Siguiente</button>
+        <button onClick={prevPage} disabled={page === 0}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+</svg></button>
+        <span> {page + 1}</span>
+        <button onClick={nextPage}><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
+</svg></button>
       </div>
     </>
   );
